2545d5ab3b8492a507241925bd28a72b268e3fa7
Nemanja Vujadinovic <vujadinovic01@gmail.com>
2022-12-03 00:41:17 +0100

    generated login component

--- HopIn/src/app/app.module.ts
+++ HopIn/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
